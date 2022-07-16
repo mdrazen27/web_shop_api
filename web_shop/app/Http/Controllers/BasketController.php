12206db49c74406aed860498066625583e58d868
Drazen Minic <mdrazen27@gmail.com>
2022-07-16 14:38:52 +0200

    made model and controller for basket

---
+++ web_shop/app/Http/Controllers/BasketController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Basket;
+use App\Http\Requests\StoreBasketRequest;
+use App\Http\Requests\UpdateBasketRequest;
+
+class BasketController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreBasketRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreBasketRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Basket  $basket
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Basket $basket)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Basket  $basket
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Basket $basket)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateBasketRequest  $request
+     * @param  \App\Models\Basket  $basket
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateBasketRequest $request, Basket $basket)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Basket  $basket
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Basket $basket)
+    {
+        //
+    }
+}
